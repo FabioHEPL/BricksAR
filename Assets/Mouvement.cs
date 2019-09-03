a6e8a4b24a9c58396b92edc58909058cad7b7f12
hozone <ho3.sharp@gmail.com>
2019-09-03 10:22:30 +0200

    Première version

---
+++ Assets/Mouvement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform from;
+
+    [SerializeField]
+    private Transform to;
+
+    [SerializeField]
+    private float time = 5f;
+
+    [SerializeField]
+    private bool bounce = false;
+
+    [SerializeField]
+    private float currentTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = from.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, to.position, time / currentTime);
+        Debug.Log(transform.position);
+    }
+}
