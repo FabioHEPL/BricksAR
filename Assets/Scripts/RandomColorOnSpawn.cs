916a4f6de3bc9ee5744a5ff490c1b29fb25da6ed
hozone <ho3.sharp@gmail.com>
2019-09-04 10:52:41 +0200

    Intégration travail max

---
+++ Assets/Scripts/RandomColorOnSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class RandomColorOnSpawn : MonoBehaviour
+{
+    Material _material;
+
+    void Awake()
+    {
+        _material = GetComponent<Renderer>().material;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        Color randomColor = Random.ColorHSV(0, 1, 0.75f, 0.75f, 0.75f, 0.75f);
+        _material.SetColor("_Color", randomColor);
+    }
+}
